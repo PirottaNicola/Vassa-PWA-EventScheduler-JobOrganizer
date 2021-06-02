8bb4ffd4a7fb736cf52fc4dc6f1b9dc534fd8edc
pirottanicola <nicolapirotta@gmail.com>
2021-06-02 19:05:17 +0200

    error fixed on BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //components
 import Calendar from "./pages/Calendar"
 function App() {
   return (
     <div className="App">
-      <Switch exact>
-        <Route path='/calendar'>
-          <Calendar />
-        </Route>
-        <Route path='*'>
-          errore: nessuna pagina disponibile a questo indirizzo
-        </Route>
-      </Switch>
+      <Router>
+        <Switch exact>
+          <Route path='/calendar'>
+            <Calendar />
+          </Route>
+          <Route path='*'>
+            <h1> errore: nessuna pagina disponibile a questo indirizzo</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
