2c0d2d853ab814ed69046a81a4890c4c7b4651cf
pirottanicola <nicolapirotta@gmail.com>
2021-06-02 21:13:57 +0200

    ervrverveverver

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 //components
 import Calendar from "./pages/Calendar"
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Calendar />
-          </Route>
-          <Route path='*'>
-            <h1> errore: nessuna pagina disponibile a questo indirizzo</h1>
-            <Link to='/'>Torna all'app</Link>
-          </Route>
-        </Switch>
-      </Router>
+    <div className='App'>
+      <Switch>
+        <Route exact path='/'>
+          <Calendar />
+        </Route>
+        <Route path='*'>
+          <h1> errore: nessuna pagina disponibile a questo indirizzo</h1>
+          <Link to='/'>Torna all'app</Link>
+        </Route>
+      </Switch>
     </div>
   );
 }
